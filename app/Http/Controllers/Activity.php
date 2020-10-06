f99ca87a1f6c9627be956cc79206586b9d61ef5b
kimcharnova <kimcharnova@gmail.com>
2020-10-06 12:00:02 +0800

    removed unnecessary comments

--- app/Http/Controllers/Activity.php
+++ app/Http/Controllers/Activity.php
@@ -9,10 +9,7 @@ class Activity extends Controller
 {
     public function index()
     {
-        // return 'Hello World';
         $activity_log = Activity_Log::all();
         return view('contacts.logs', compact('activity_log'));
-
-        // return view('contacts.logs', ['activity_log' => $activity_log]);
     }
 }
